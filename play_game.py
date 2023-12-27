365ac8eabd6d7051e9de46ea06ac75975f57611c
joshweiss <jweiss32@gmail.com>
2023-12-27 20:03:23 +0200

    Finished multiplayer game

--- play_game.py
+++ play_game.py
@@ -3,21 +3,24 @@ from funcs import *
 
 
 def start_game():
-    board = create_board()
     game_continues = True
     players = []
-    cols_completed = {}
-    num_players = int(input('How many players are there?'))
+    player_boards = {}
+    num_players = int(input('How many players are there? '))
     for i in range(1, num_players+1):
-        name = input('What is your name?')
+        name = input('What is your name? ')
         players.append(name)
-        cols_completed[name] = 0
+        player_boards[name] = create_board()
     while game_continues:
         for player in players:
             print(f'It\'s {player}\'s turn!')
-            board = take_turn(player, board)
-        cols_completed = sum(1 for v in board.values() if v == 'Complete')
-    print('You Win!')
+            player_boards = take_turn(player, player_boards)
+            cols_completed = sum(1 for v in player_boards[player].values() if v == f'{player} Completed')
+            if cols_completed >= 3:
+                print(f'{player} Wins!')
+                game_continues = False
+                break
+    print('Thanks for Playing!')
 
 
 if __name__ == "__main__":
--- take_turn.py
+++ take_turn.py
@@ -4,7 +4,7 @@ import copy
 from funcs import *
 
 
-FULL_BOARD = {2:3, 3:5, 4:7, 5:9, 6:1, 7:3, 8:1, 9:9, 10:7, 11:5, 12:3}
+FULL_BOARD = {2:3, 3:5, 4:7, 5:9, 6:11, 7:13, 8:11, 9:9, 10:7, 11:5, 12:3}
 
 
 def roll_dice():
@@ -22,7 +22,7 @@ def show_dice_roll(dice_rolled, summed_options, temp_nums, board):
     print(f'You rolled: {dice_rolled}')
     print(f'Your temporary numbers are: {temp_nums}')
     for col in board:
-        if board[col] == 'Complete':
+        if isinstance(board[col], str):
             summed_options = remove_occurrences(summed_options, col)
             summed_options = [x for x in summed_options if x != []]
     open_spots = 3 - len(temp_nums)
@@ -59,7 +59,7 @@ def show_dice_roll(dice_rolled, summed_options, temp_nums, board):
         print('Therefore your options are:')
         for i, new_option in enumerate(new_summed_options, 1):
             print(f'Option {i}: {new_option}')
-        chosen_option = int(input('Which option would you like to choose?\n'))
+        chosen_option = int(input('Which option would you like to choose? '))
         chosen_sums = new_summed_options[chosen_option-1]
         return chosen_sums
     else:
@@ -67,38 +67,49 @@ def show_dice_roll(dice_rolled, summed_options, temp_nums, board):
         return -1
 
 
-def update_board(board_dict, chosen_sums):
-    for val in chosen_sums:
-        if board_dict[val] != 'Complete':
-            board_dict[val] += 1
-            if board_dict[val] >= FULL_BOARD[val]:
-                board_dict[val] = 'Complete'
-    return board_dict
+def update_board(board_dict, chosen_sums, current_player, completed_cols):
+    for col in chosen_sums:
+        if isinstance(board_dict[col], int):
+            board_dict[col] += 1
+            if board_dict[col] >= FULL_BOARD[col]:
+                board_dict[col] = f'{current_player} Completed'
+                completed_cols.append(col)
+    return board_dict, completed_cols
 
 
-def take_turn(player, board):
+def take_turn(current_player, player_boards):
     cont = 1
     temp_nums = []
-    orig_board = copy.deepcopy(board)
-    print('The Current Board is:')
-    print(board)
+    completed_cols = []
+    temp_board = copy.deepcopy(player_boards[current_player])
+    print(f'{current_player}\'s Board: {player_boards[current_player]}')
+    for player in player_boards:
+        if player != current_player:
+            print(f'{player}\'s Board: {player_boards[player]}')
     while cont:
         dice_rolled, summed_options = roll_dice()
-        chosen_sums = show_dice_roll(dice_rolled, summed_options, temp_nums, board)
+        chosen_sums = show_dice_roll(dice_rolled, summed_options, temp_nums, temp_board)
         if chosen_sums == -1:
-            print('orig board is')
-            print(orig_board)
-            return orig_board
+            return player_boards
         for chosen_val in chosen_sums:
             if chosen_val not in temp_nums:
                 temp_nums.append(chosen_val)
                 temp_nums.sort()
-        board = update_board(board, chosen_sums)
-        print(f'This is the new board:\n{board}')
-        cont = int(input('To continue press 1, to save progress press 0\n'))
-    return board
+        temp_board, completed_cols = update_board(temp_board, chosen_sums, current_player, completed_cols)
+        print(f'Your Temporary Numbers: {temp_nums}')
+        print(f'{current_player}\'s Board: {temp_board}')
+        for player in player_boards:
+            if player != current_player:
+                print(f'{player}\'s Board: {player_boards[player]}')
+        cont = int(input('To continue press 1, to end turn and save progress press 0: '))
+
+    for player in player_boards:
+        if player != current_player:
+            for val in completed_cols:
+                player_boards[player][val] = f'{current_player} Completed'
+        else:
+            player_boards[player] = temp_board
+    return player_boards
 
 
-if __name__ == "__main__":
-    take_turn(create_board())
 
